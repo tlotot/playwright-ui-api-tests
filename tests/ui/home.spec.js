ddb2eef84092f1be557dc27b74d32ee2231ec68b
Tanya Lototska <friends2001@ukr.net>
2026-04-11 21:27:35 +0300

    chore(home): use baseURL instead of hardcoded URL in home.spec.js

--- tests/ui/home.spec.js
+++ tests/ui/home.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '../setup';
 
 test('Smoke: home page should open and display navigation', async ({ page }) => {
-  await page.goto('https://automationexercise.com/');
+  await page.goto('/');
 
   await expect(page).toHaveURL(/automationexercise/);
   await expect(page).toHaveTitle(/Automation Exercise/);
